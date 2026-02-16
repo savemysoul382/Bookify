2e38d1d389d82fe01b7a7076c5864031fd97b834
savemysoul382 <serjke88@gmail.com>
2026-02-16 14:34:59 +0300

    Добавьте файлы проекта.

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,13 @@
+﻿// Bookify.Domain
+
+namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; init; }
+}
