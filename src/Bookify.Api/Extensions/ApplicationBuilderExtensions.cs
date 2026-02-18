f7d8b019b37e6e3b02f3d3b6c5b98caee21698df
savemysoul382 <serjke88@gmail.com>
2026-02-18 16:34:33 +0300

    Presentation layer

--- src/Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -10,10 +10,10 @@ public static class ApplicationBuilderExtensions
     {
         using var scope = app.ApplicationServices.CreateScope();
 
-        using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-        dbContext.Database.Migrate();
-    }
+            dbContext.Database.Migrate();
+        }
 
     public static void UseCustomExceptionHandler(this IApplicationBuilder app)
     {
--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Bookify API", Version = "v1" });
+});
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -18,11 +21,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bookify API v1");
+       
+        // c.RoutePrefix = "swagger";
+    });
 
     app.ApplyMigrations();
 
-    app.SeedData(); // need to run once
+    // app.SeedData(); // need to run once
 }
 
 app.UseHttpsRedirection();
