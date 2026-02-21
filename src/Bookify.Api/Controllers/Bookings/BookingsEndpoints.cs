89b1e59924f7cec676c2a79f3680f1b0bf1f1dca
savemysoul382 <serjke88@gmail.com>
2026-02-21 18:16:27 +0300

    Minimal API

---
+++ src/Bookify.Api/Controllers/Bookings/BookingsEndpoints.cs
@@ -0,0 +1,54 @@
+﻿using Bookify.Application.Bookings.GetBooking;
+using Bookify.Application.Bookings.ReserveBooking;
+using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Api.Controllers.Bookings
+{
+    public static class BookingsEndpoints
+    {
+        public static IEndpointRouteBuilder MapBookingEndpoints(this IEndpointRouteBuilder builder)
+        {
+            builder.MapGet("bookings/{id}", GetBooking)
+                .RequireAuthorization() // RequireAuthorization("bookings:read")?
+                .WithName(nameof(GetBooking));
+
+            builder.MapPost("bookings", ReserveBooking)
+                .RequireAuthorization(); // RequireAuthorization("bookings:write")?
+            return builder;
+        }
+
+
+        public static async Task<IResult> GetBooking(Guid id, ISender sender, CancellationToken cancellationToken)
+        {
+            GetBookingQuery query = new GetBookingQuery(id);
+
+            Result<BookingResponse> result = await sender.Send(query, cancellationToken);
+            return result.IsSuccess
+                ? Results.Ok(result.Value)
+                : Results.NotFound();
+        }
+
+        public static async Task<IResult> ReserveBooking(
+            ReserveBookingRequest request,
+            ISender sender,
+            CancellationToken cancellationToken)
+        {
+            ReserveBookingCommand command = new ReserveBookingCommand(
+                request.ApartmentId,
+                request.UserId,
+                request.StartDate,
+                request.EndDate);
+
+            Result<Guid> result = await sender.Send(command, cancellationToken);
+
+            if (result.IsFailure)
+            {
+                return Results.BadRequest(result.Error);
+            }
+
+            //return CreatedAtAction(nameof(GetBooking), new {id = result.Value}, result.Value); this for Controller
+            return Results.CreatedAtRoute(nameof(GetBooking), new {id = result.Value}, result.Value);
+        }
+    }
+}
--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,4 +1,7 @@
+using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
+using Asp.Versioning.Builder;
+using Bookify.Api.Controllers.Bookings;
 using Bookify.Api.Extensions;
 using Bookify.Api.OpenApi;
 using Bookify.Application;
@@ -62,6 +65,15 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .ReportApiVersions()
+    .Build();
+
+RouteGroupBuilder routeBuilder = app.MapGroup("api/v{version:apiVersion}").WithApiVersionSet(apiVersionSet);
+
+routeBuilder.MapBookingEndpoints();
 
 app.MapHealthChecks(
     "health",
